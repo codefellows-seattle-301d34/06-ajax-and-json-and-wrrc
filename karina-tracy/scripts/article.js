92a77ed5bf807bf66e4fad5cfd37711f4b36c91d
Karina Amador <akarina.at@gmail.com>
2018-05-15 10:45:40 -0700

    Answered comments and solves the last if/else statement

---
+++ karina-tracy/scripts/article.js
@@ -0,0 +1,60 @@
+'use strict';
+
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
+
+// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+Article.all = [];
+
+// COMMENT: Why isn't this method written as an arrow function?
+// Because it is using contextual this.
+Article.prototype.toHtml = function() {
+  let template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // Not sure? Check the docs!
+  // The question mark and colon are ternary operator. This works as the if, else statements. The question mark works as the if and the colon would be the else. So if something is true (?) then do this (:) if its not do this.
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+// REVIEW: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
+
+// REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
+
+// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// .loadAll() is called in the .fetchAll() function. Before .fecthAll() function, rawData was an array of objects (our main data). In .fetchAll() function, rawData becomes a value of the local storage.
+Article.loadAll = articleData => {
+  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+
+  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+}
+
+// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+Article.fetchAll = () => {
+  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  //The if statement is checking is the local storage has data. rawData was set to local storage in the callback function passed to $.getJSON.
+  if (localStorage.rawData) {
+
+    Article.loadAll(JSON.parse(localStorage.rawData));
+
+  } else {
+    $.getJSON('hackerIpsum.json')
+      .then(data => {
+        console.log('from .then', data);
+        localStorage.setItem('rawData', JSON.stringify(data));
+        Article.loadAll(data)
+
+      })
+  }
+}
